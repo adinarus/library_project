c4dacd0ad764eda1d58a8d8fb5ecae469e28e4b2
MagdaIonescu <mionescu719@gmail.com>
2023-12-30 01:08:55 +0200

    Create the User entity.

---
+++ src/main/java/com/library/library_project/entities/User.java
@@ -0,0 +1,79 @@
+package com.library.library_project.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Collection;
+
+@Entity
+@Table (name="users")
+public class User {
+    @Column(name = "id")
+    private int id;
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue
+    public int getId() {
+        return id;
+    }
+    @Column(name = "firstname")
+    @Basic
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Column(name = "lastname")
+    @Basic
+    private String surname;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+
+    @Column(name = "username")
+    @Basic
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
