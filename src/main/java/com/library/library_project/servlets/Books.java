ec4e9d8e5e76155bc6c0377cc46e673dbbeebd1e
Anna1070 <anapaula.luca@ulbsibiu.ro>
2024-01-05 15:42:25 +0200

    Implemented the Session Bean and also fixed some mini errors

--- src/main/java/com/library/library_project/servlets/Books.java
+++ src/main/java/com/library/library_project/servlets/Books.java
@@ -1,15 +1,23 @@
 package com.library.library_project.servlets;
 
+import com.library.library_project.dto.BookDto;
+import com.library.library_project.ejb.BookBean;
+import jakarta.inject.Inject;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "Books", value = "/Books")
 public class Books extends HttpServlet {
+    @Inject
+    BookBean bookBean;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<BookDto> books=bookBean.findAllBooks();
+        request.setAttribute("books",books);
         request.setAttribute("numberOfAvailableBooks",2);
         request.getRequestDispatcher("/WEB-INF/pages/books.jsp").forward(request,response);
     }
